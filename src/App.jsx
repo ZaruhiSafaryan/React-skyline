1c83cddcc541fd75b833a769cecb87e9bdeb8906
Artyom <artomdavtan1@gmail.com>
2023-12-01 18:41:10 +0300

    dsds

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div>
       <h1>React repo</h1>
-      <span>nver</span>
+      <span>nver2</span>
       <p>Lorem ipsum dolor sit amet.</p>
       <p>Lorem ipsum dolor sit amet.</p>
     </div>

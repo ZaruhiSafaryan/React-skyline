b177a41c0a5d0462c1ea758dc614f7af1ecac1b2
ZaruhiSafaryan <zaruhi92safaryan@gmail.com>
2023-12-04 15:18:44 +0300

    added my first homework

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ export default function App() {
   return (
     <div>
       <h1>React repo</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla error consequuntur aut eius alias nesciunt quibusdam impedit recusandae rem voluptas.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
     </div>
   )
 }
